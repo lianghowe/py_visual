ea2007a24f26c4136c0b4e55206203aacba70d1c
lianghowe <littlesimple@foxmail.com>
2018-04-04 22:22:35 +0800

    Initial commit

---
+++ scatter_squares.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import pygame
+
+x_values = list(range(1, 1001))
+y_values = [x ** 2 for x in x_values]
+
+plt.scatter(x_values, y_values, c = y_values, cmap = plt.cm.Blues,
+ edgecolor = 'none', s = 40)
+#plt.scatter(x_values, y_values, s = 40)
+#plt.scatter(x_values, y_values, edgecolor = 'none', s = 40)
+
+# 设置图表标题并给坐标轴加上标签
+plt.title("Square Numbers", fontsize = 24)
+plt.xlabel("Value", fontsize = 14)
+plt.ylabel("Square of Value", fontsize = 14)
+
+# 设置刻度标记的大小
+plt.tick_params(axis = 'both', which = 'major', labelsize = 14)
+
+# 设置每个坐标轴的取值范围
+plt.axis([0, 1100, 0, 1100000])
+
+#plt.show()
+plt.savefig('squares_plot.png', bbox_inches = 'tight')
+
+pygame.init()
+
+if pygame.event.get() == pygame.K_q:
+			sys.exit()		
